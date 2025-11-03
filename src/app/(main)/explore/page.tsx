a88299ad7d9db9a28f274a7abe1b40108274b8f7
camone <106678583+camoneart@users.noreply.github.com>
2025-11-03 11:54:08 +0900

    add: Suspense

--- src/app/(main)/explore/page.tsx
+++ src/app/(main)/explore/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { Metadata } from "next";
 import { getPageMetadata } from "@/config/metadata";
 import styles from "./ExplorePage.module.css";
@@ -11,7 +12,13 @@ const ExplorePage = () => {
 			<div className={styles["title-bg"]}></div>
 			<h1 className={`${styles["explore-title"]}`}>記事探索</h1>
 			<div className={`${styles["explorer-container"]}`}>
-				<Explore.ExplorePageClient />
+				<Suspense
+					fallback={
+						<div className="grid place-items-center">読み込み中...</div>
+					}
+				>
+					<Explore.ExplorePageClient />
+				</Suspense>
 			</div>
 		</>
 	);
