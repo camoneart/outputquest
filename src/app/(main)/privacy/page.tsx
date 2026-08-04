9c1e45c2710ea01fc0f7a93c32835a88c101596f
Dende <106678583+dendedev@users.noreply.github.com>
2026-08-05 03:20:47 +0900

    chore(rename): kaijutale を dendedev に統一 (#226)
    Co-authored-by: kaijutale <106678583+kaijutale@users.noreply.github.com>

--- src/app/(main)/privacy/page.tsx
+++ src/app/(main)/privacy/page.tsx
@@ -92,7 +92,7 @@ const PrivacyPage = () => {
 										<dt className="md:text-base text-sm">開発者のメールアドレス：</dt>
 										<dd>
 											<SafeMailtoLink
-												user="taleofkaiju"
+												user="ldendedevl"
 												domain="gmail.com"
 												className="md:text-base text-sm underline underline-offset-4 py-1 pr-2"
 											/>
@@ -102,12 +102,12 @@ const PrivacyPage = () => {
 										<dt className="md:text-base text-sm">開発者のX：</dt>
 										<dd>
 											<Link
-												href="https://x.com/kaijutale"
+												href="https://x.com/dendedev"
 												className="md:text-base text-sm underline underline-offset-4 py-1 pr-2"
 												target="_blank"
 												rel="noopener noreferrer"
 											>
-												@kaijutale
+												@dendedev
 											</Link>
 										</dd>
 									</div>
